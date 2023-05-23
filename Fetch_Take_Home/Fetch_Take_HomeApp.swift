0c8952ed22c6fbdb74cbc3bb5a51952aa22d8325
MrKearns <jkearns631@students.mchenry.edu>
2023-05-23 14:41:13 -0500

    Initial Commit

---
+++ Fetch_Take_Home/Fetch_Take_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_HomeApp.swift
+//  Fetch_Take_Home
+//
+//  Created by Jonathan Kearns on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Take_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
